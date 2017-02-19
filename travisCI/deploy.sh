94c60cd5ac4e55b1dad1a1cdd8106e59e5bc4141
Mitchell Krog <mitchellkrog@gmail.com>
2017-02-19 13:43:41 +0200

    Change Build and Version numbering structure.
    - Release Names / Versions now dated and easier for users to keep track of.

--- travisCI/deploy.sh
+++ travisCI/deploy.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
-YEAR=$(date +"%Y")
-MONTH=$(date +"%m")
+BUILDDATE=$(date +"%Y-%m-%d")
 git config --global user.email "${GIT_EMAIL}"
 git config --global user.name "${GIT_NAME}"
 git config --global push.default simple
 git remote add origin https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git
-export GIT_TAG=2.$YEAR-$MONTH.$TRAVIS_BUILD_NUMBER
+export GIT_TAG=$BUILDDATE.Build-$TRAVIS_BUILD_NUMBER
 git fetch --tags
 msg="Tag Generated from TravisCI for build $TRAVIS_BUILD_NUMBER"
 if git tag $GIT_TAG -a -m "$msg" 2>/dev/null; then
