9123b43d389511ae279e29ff44300b9f48d27caa
Mitchell Krog <mitchellkrog@gmail.com>
2017-06-01 16:49:23 +0200

    Fixes to changefile.sh script

--- travisCI/change-file.sh
+++ travisCI/change-file.sh
@@ -25,7 +25,8 @@ sudo $TRAVIS_BUILD_DIR/travisCI/modify-globalblacklist.sh
 sudo chown -R travis:travis $TRAVIS_BUILD_DIR/*
 
 # Add the modified file to the and commit it
-git add $TRAVIS_BUILD_DIR/globalblacklist.conf
+git add $TRAVIS_BUILD_DIR/conf.d/globalblacklist.conf
+git add $TRAVIS_BUILD_DIR/Engintron_for_cPanel_WHM_Configuration_Example/etc/nginx/conf.d/globalblacklist.conf
 git commit -am "V3.$YEAR.$MONTH.$TRAVIS_BUILD_NUMBER [ci skip]"
 
 # Travis now moves to the before_deploy: section of .travis.yml
