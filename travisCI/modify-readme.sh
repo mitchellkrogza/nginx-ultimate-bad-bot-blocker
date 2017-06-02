59fc0989ce048054701a43dcc1892ef8dc21fe54
Mitchell Krog <mitchellkrog@gmail.com>
2017-06-02 11:10:05 +0200

    Formatting of Version Information Block in README.md

--- travisCI/modify-readme.sh
+++ travisCI/modify-readme.sh
@@ -14,8 +14,8 @@ _inputdbA=/tmp/lastupdated.db
 _tmpnginxA=tmpnginxA
 
 # Start and End Strings to Search for to do inserts into template
-_startmarker="********************************************"
-_endmarker="--------------------------------------------"
+_startmarker="##### Version Information #"
+_endmarker="##### Version Information ##"
 
 # PRINT VERSION INFORMATION INTO README.md
 # ****************************************
@@ -25,18 +25,18 @@ now="$(date)"
 end=$(date +%s.%N)    
 echo $_startmarker >> $_tmpnginxA
 runtime=$(python -c "print(${end} - ${start})")
-printf "> Version: "$MY_GIT_TAG"" >> $_tmpnginxA
+printf "********************************************\n> Version: "$MY_GIT_TAG"\n********************************************\n" >> $_tmpnginxA
 echo $_endmarker  >> $_tmpnginxA
 IFS=$LASTUPDATEIFS
 mv $_tmpnginxA $_inputdbA
 ed -s $_inputdbA<<\IN
-1,/********************************************/d
-/--------------------------------------------/,$d
+1,/##### Version Information #/d
+/##### Version Information ##/,$d
 ,d
 .r /home/travis/build/mitchellkrogza/nginx-ultimate-bad-bot-blocker/README.md
-/********************************************/x
+/##### Version Information #/x
 .t.
-.,/--------------------------------------------/-d
+.,/##### Version Information ##/-d
 #,p
 #,p used to print output replaced with w below to write
 w /home/travis/build/mitchellkrogza/nginx-ultimate-bad-bot-blocker/README.md
@@ -46,4 +46,3 @@ rm $_inputdbA
 
 
 exit 0
-
