68c0560e10fa041b39cfa73815443e7ec51abf8f
Mitchell Krog <mitchellkrog@gmail.com>
2017-06-02 10:06:43 +0200

    Write Build / Version Number into README.md

--- travisCI/modify-readme.sh
+++ travisCI/modify-readme.sh
@@ -13,26 +13,26 @@ _inputdbA=/tmp/lastupdated.db
 _tmpnginxA=tmpnginxA
 
 # Start and End Strings to Search for to do inserts into template
-_startmarker=">>>"
-_endmarker="<<<"
+_startmarker="#>>>#"
+_endmarker="#<<<#"
 
 # PRINT VERSION INFORMATION INTO GLOBALBLACKLIST FILE 1
 # *****************************************************
 VERSIONUMBER=$IFS
 IFS=$'\n'
 echo $_startmarker >> $_tmpnginxA
-printf " Version: "$MY_GIT_TAG "" >> $_tmpnginxA
+printf " "$MY_GIT_TAG "" >> $_tmpnginxA
 echo $_endmarker  >> $_tmpnginxA
 IFS=$VERSIONUMBER
 mv $_tmpnginxA $_inputdbA
 ed -s $_inputdbA<<\IN
-1,/>>>/d
-/<<</,$d
+1,/#>>>#/d
+/#<<<#/,$d
 ,d
 .r /home/travis/build/mitchellkrogza/nginx-ultimate-bad-bot-blocker/README.md
-/>>>/x
+/#>>>#/x
 .t.
-.,/<<</-d
+.,/#<<<#/-d
 #,p
 #,p used to print output replaced with w below to write
 w /home/travis/build/mitchellkrogza/nginx-ultimate-bad-bot-blocker/README.md
