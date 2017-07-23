53a7deb9d2ad0627e9a6c43e75420a6821855ab4
Mitchell Krog <mitchellkrog@gmail.com>
2017-07-23 11:33:44 +0200

    Test pushing bad-referrers.list to another repo.

--- .dev-tools/modify-files-and-commit.sh
+++ .dev-tools/modify-files-and-commit.sh
@@ -95,19 +95,6 @@ tar -czf bots.d.tar.gz -C $TRAVIS_BUILD_DIR/bots.d/ .
 git add -A
 git commit -am "V3.$YEAR.$MONTH.$TRAVIS_BUILD_NUMBER [ci skip]"
 
-# ***************************************************
-# Try pushing changes to Google Ghost Spam Repository
-# ***************************************************
-
-cd /home/travis/build/mitchellkrogza/
-sudo git clone https://github.com/mitchellkrogza/Stop.Google.Analytics.Ghost.Spam.HOWTO.git
-sudo cp $TRAVIS_BUILD_DIR/_generator_lists/bad-referrers.list /home/travis/build/mitchellkrogza/Stop.Google.Analytics.Ghost.Spam.HOWTO/.dev-tools/_input_source/bad-referrers.list
-cd /home/travis/build/mitchellkrogza/Stop.Google.Analytics.Ghost.Spam.HOWTO/
-git remote rm origin
-git remote add origin https://w7FDb+tofB5e+ej2GQDvOtlI75wADWuEsgYKCbiFnC4YKLGaN/NyNaAMuOdCPaLL6yWieHCTOQyG/zjVxZe8ST4R+2XQHv1x1PldbEHKnox+bAnrB9n3qZ0a8AOBtceI33gbrpL1wnZiVdcCh5HFlhvEOsiXhg+tc4BaIUZnA981dcf0EmIvWHWesmrgHVVh7x5Isgs5ecEpxyZZqn8FkHMgTnYe2s3IIlcIQhdvK2AhI33zz0fb1B9V/ll6UtAmbLKrYcN7waw/ZRzRDxuzJbCV6VCCFKzXo2DEs5NlEoa0P2bJAEj9dS5Q+1DGK5u1llbnycUBaS0phURb3gg8bcjX/9Ovlz/qnpw9EfimGxE7T6WjfI3sOQ9mFszgs/BigtZkay4ijGRNejrSiyC+/du/B0srto032HD8LTJBkbW5/OW9sr2JDmnOHJiceks4C8WYBVgvslLfnTD6tDgvJ88kjpk33Juq5ULm5ZcsJNQn+Dvn1btWQc1y9aRfktmyrTHoGTUXTx6c/YWXSdhuXVGCGgwxQ6PoOmhNeETuCKaA0dwoK1sACvgxJRDZCQpj7DneVGl7qqwr7LHQMrDMID+iXfr6ZMbFtC4HmK1PaDSxnPON/ecvEJLbUVRheo/70lYCYj2rlc7ytAUvMZ29Hzc353/GqF5FqxzX4kEubi8=@github.com/Stop.Google.Analytics.Ghost.Spam.HOWTO.git
-git add -A
-git commit -am "Referrers (+)"
-sudo git push origin master
 
 # *************************************************************
 # Travis now moves to the before_deploy: section of .travis.yml
