d13447afddf4e1ac8b869b40c38151d1b792a23e
Mitchell Krog <mitchellkrog@gmail.com>
2019-06-20 17:49:47 +0200

    Update regex on test version Ref: #264

--- .dev-tools/generate-blacklist-testing-of-changes.sh
+++ .dev-tools/generate-blacklist-testing-of-changes.sh
@@ -237,8 +237,8 @@ rm ${_inputdb3}
 printf '%s\n' "$_start4" >> ${_tmpnginx4}
 while IFS= read -r LINE
 do
-printf '\t"~*%s%s"\t\t%s\n' "${LINE}" "\b" "$_action4" >> ${_tmpnginx4}
-printf '\t"~*%s"\t\t%s\n' "${LINE}" "$_action4" >> ${_tmpnginx4}
+printf '\t"~*%s%s"\t\t%s\n' "${LINE}" "(?:|\b)" "$_action4" >> ${_tmpnginx4}
+#printf '\t"~*%s"\t\t%s\n' "${LINE}" "$_action4" >> ${_tmpnginx4}
 done < ${_input4}
 printf '%s\n' "$_end4"  >> ${_tmpnginx4}
 mv ${_tmpnginx4} ${_inputdb4}
