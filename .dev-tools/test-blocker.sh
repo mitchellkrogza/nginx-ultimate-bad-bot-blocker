1cf27a505cd64ee48cc0ce184f50cce0e23ac89a
Mitchell Krog <mitchellkrog@gmail.com>
2019-06-22 20:20:44 +0200

    Disable array test

--- .dev-tools/test-blocker.sh
+++ .dev-tools/test-blocker.sh
@@ -259,12 +259,12 @@ run_curltest15
 
 #IFS=$'\n' read -d '' -r -a lines < ${TRAVIS_BUILD_DIR}/_generator_lists/bad-user-agents.list
 #echo "${lines[@]}"
-IFS=$'\n'
-file=${TRAVIS_BUILD_DIR}/_generator_lists/bad-user-agents.list
-lines=`cat $file`
-for line in $lines; do
-        curl -v -A "$line" http://localhost:9000 2>&1
-done
+#IFS=$'\n'
+#file=${TRAVIS_BUILD_DIR}/_generator_lists/bad-user-agents.list
+#lines=`cat $file`
+#for line in $lines; do
+#        curl -v -A "$line" http://localhost:9000 2>&1
+#done
 
 
 echo "Tests Completed"
