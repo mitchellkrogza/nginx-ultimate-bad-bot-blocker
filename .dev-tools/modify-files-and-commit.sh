4f9ed8b6d1e001535f289ce9237ccb3e881dae63
Mitchell Krog <mitchellkrog@gmail.com>
2019-06-28 13:51:52 +0200

    Update Commit Script [MASTER]

--- .dev-tools/modify-files-and-commit.sh
+++ .dev-tools/modify-files-and-commit.sh
@@ -16,21 +16,22 @@
 #                                                                            #
 ##############################################################################                                                                
 
+# ------------------------------------------------------------------------------
 # MIT License
-
+# ------------------------------------------------------------------------------
 # Copyright (c) 2017 Mitchell Krog - mitchellkrog@gmail.com
 # https://github.com/mitchellkrogza
-
+# ------------------------------------------------------------------------------
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
 # in the Software without restriction, including without limitation the rights
 # to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 # copies of the Software, and to permit persons to whom the Software is
 # furnished to do so, subject to the following conditions:
-
+# ------------------------------------------------------------------------------
 # The above copyright notice and this permission notice shall be included in all
 # copies or substantial portions of the Software.
-
+# ------------------------------------------------------------------------------
 # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 # IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 # FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -38,91 +39,86 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
+# ------------------------------------------------------------------------------
 
-# ******************
-# Set Some Variables
-# ******************
+# ------------------------
+# Set Terminal Font Colors
+# ------------------------
+
+bold=$(tput bold)
+red=$(tput setaf 1)
+green=$(tput setaf 2)
+yellow=$(tput setaf 3)
+blue=$(tput setaf 4)
+magenta=$(tput setaf 5)
+cyan=$(tput setaf 6)
+white=$(tput setaf 7)
+defaultcolor=$(tput setaf default)
+
+# ---------
+# Variables
+# ---------
 
 YEAR=$(date +"%Y")
 MONTH=$(date +"%m")
 cd ${TRAVIS_BUILD_DIR}
 
-# *******************
-# RELEASE NEW VERSION
-# *******************
+# ---------
+# FUNCTIONS
+# ---------
+
+releaseNewVersion () {
 latestbuild=V4.${YEAR}.${MONTH}.${TRAVIS_BUILD_NUMBER}
 printf "\n"
 echo "${bold}${green}All Nginx Tests Completed"
 echo "${bold}${green}All Bot and Referrer Testing Completed"
 echo "${bold}${green}All Function Testing Completed"
 echo "${bold}${magenta}Releasing ${latestbuild}"
+}
 
-# *******************************
-# Remove Remote Added by TravisCI
-# *******************************
-
+commitBuildChanges () {
 git remote rm origin
-
-# **************************
-# Add Remote with Secure Key
-# **************************
-
 git remote add origin https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git
-
-# *********************
-# Set Our Git Variables
-# *********************
-
 git config --global user.email "${GIT_EMAIL}"
 git config --global user.name "${GIT_NAME}"
 git config --global push.default simple
-
-# *******************************************
-# Make sure we have checked out master branch
-# *******************************************
-
 git checkout master
-
-# ***************************************************************
-# Gzip Our Latest Release So We can Include it the Travis Release
-# ***************************************************************
-
 cd ${TRAVIS_BUILD_DIR}/.latest_release/
 tar -czf conf.d.tar.gz -C ${TRAVIS_BUILD_DIR}/conf.d/ .
 tar -czf bots.d.tar.gz -C ${TRAVIS_BUILD_DIR}/bots.d/ .
-
-# *************************************
-# Add all the modified files and commit
-# *************************************
-
 git add -A
 git commit -am "V4.${YEAR}.${MONTH}.${TRAVIS_BUILD_NUMBER} [ci skip]"
+}
 
-# *************************************************************
-# Travis now moves to the before_deploy: section of .travis.yml
-# *************************************************************
+# -------------
+# Run Functions
+# -------------
 
-# **********************
+releaseNewVersion
+commitBuildChanges
+
+# ----------------------
 # Exit With Error Number
-# **********************
+# ----------------------
 
 exit ${?}
 
+# ------------------------------------------------------------------------------
 # MIT License
-
+# ------------------------------------------------------------------------------
 # Copyright (c) 2017 Mitchell Krog - mitchellkrog@gmail.com
 # https://github.com/mitchellkrogza
-
+# ------------------------------------------------------------------------------
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
 # in the Software without restriction, including without limitation the rights
 # to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 # copies of the Software, and to permit persons to whom the Software is
 # furnished to do so, subject to the following conditions:
-
+# ------------------------------------------------------------------------------
 # The above copyright notice and this permission notice shall be included in all
 # copies or substantial portions of the Software.
-
+# ------------------------------------------------------------------------------
 # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 # IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 # FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -130,3 +126,4 @@ exit ${?}
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
+# ------------------------------------------------------------------------------
