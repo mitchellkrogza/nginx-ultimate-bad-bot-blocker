8b2c522d1c1ef4810399effa42f0fe3a107f47ba
Mitchell Krog <mitchellkrog@gmail.com>
2021-07-05 08:53:56 +0200

    GHA workflow - Fix Test 4 [skip travis]

--- dev-tools/test-blocker-whitelist-ips.sh
+++ dev-tools/test-blocker-whitelist-ips.sh
@@ -56,7 +56,8 @@ magenta=$(tput setaf 5)
 cyan=$(tput setaf 6)
 white=$(tput setaf 7)
 defaultcolor=$(tput setaf default)
-thisip=$(dig +short myip.opendns.com @resolver1.opendns.com)
+#thisip=$(dig +short myip.opendns.com @resolver1.opendns.com)
+thisip=$(ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1')
 
 # ---------
 # FUNCTIONS
