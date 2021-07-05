fe51247d018ad827a5b725d905ae68555b7b0f5b
Mitchell Krog <mitchellkrog@gmail.com>
2021-07-05 09:51:57 +0200

    GHA workflow - Full Test Stage 1 TESTS [skip travis]

--- dev-tools/test-blocker-false-positives.sh
+++ dev-tools/test-blocker-false-positives.sh
@@ -127,7 +127,7 @@ UAtest_mustmatch () {
 for mustmatch in "${UAmustmatch[@]}"
 do
    if
-   curl -A "${mustmatch}" http://localhost:80 2>&1 | grep -i 'Empty reply'; then
+   curl -A "${mustmatch}" http://localhost:80 2>&1 | grep -i '(52))'; then
    echo "${bold}${green}PASSED - ${red}${mustmatch} was ${bold}${red}BLOCKED"
    else
    echo "${bold}${red}FAILED - ${red}${mustmatch} was ${bold}${red}NOT BLOCKED"
