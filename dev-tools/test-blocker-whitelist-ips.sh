399f91f15a5463855921f7bb152192e0d503744a
Mitchell Krog <mitchellkrog@gmail.com>
2021-07-05 09:25:52 +0200

    GHA workflow - Debug blacklist-ips.conf [skip travis]

--- dev-tools/test-blocker-whitelist-ips.sh
+++ dev-tools/test-blocker-whitelist-ips.sh
@@ -57,7 +57,7 @@ cyan=$(tput setaf 6)
 white=$(tput setaf 7)
 defaultcolor=$(tput setaf default)
 #thisip=$(dig +short myip.opendns.com @resolver1.opendns.com)
-thisip=$(ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1')
+thisip=$(ip -o route get to 8.8.8.8 | sed -n 's/.*src \([0-9.]\+\).*/\1/p')
 
 # ---------
 # FUNCTIONS
