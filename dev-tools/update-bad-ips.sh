9e2d63521c7c1a8f7e76ddd612118e41453ef4cd
Mitchell Krog <mitchellkrog@gmail.com>
2025-11-24 09:14:43 +0200

    Update update-bad-ips.sh
    Prevent outdated certs from failing the IPDB updates

--- dev-tools/update-bad-ips.sh
+++ dev-tools/update-bad-ips.sh
@@ -59,7 +59,7 @@ tmp="./_generator_lists/tmp.list"
 # ---------
 
 fetchBadIPs () {
-    sudo wget https://hosts.ubuntu101.co.za/ngxubbb/bad-ips.list -O ${outputfile}
+    sudo wget --no-check-certificate  https://hosts.ubuntu101.co.za/ngxubbb/bad-ips.list -O ${outputfile}
 echo "${bold}${green}----------------------------"
 echo "${bold}${green}Fetching Latest BAD IPs List"
 echo "${bold}${green}----------------------------"
